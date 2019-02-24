c6c5772e9fce29007573d793fd9d6e8357076c1d
npiparav <ndpiparava@gmail.com>
2019-02-24 15:11:22 +0530

    Chapter 2 - NSManagedObject Subclasses

---
+++ Chapter 2 - NSManagedObject Subclasses/Bow Ties/Bowtie+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Bowtie+CoreDataClass.swift
+//  Bow Ties
+//
+//  Created by npiparav on 23/02/19.
+//  Copyright © 2019 Razeware. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Bowtie: NSManagedObject {
+
+}
---
+++ Chapter 2 - NSManagedObject Subclasses/Bow Ties/Bowtie+CoreDataProperties.swift
@@ -0,0 +1,29 @@
+//
+//  Bowtie+CoreDataProperties.swift
+//  Bow Ties
+//
+//  Created by npiparav on 23/02/19.
+//  Copyright © 2019 Razeware. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Bowtie {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Bowtie> {
+        return NSFetchRequest<Bowtie>(entityName: "Bowtie")
+    }
+
+    @NSManaged public var isFavorite: Bool
+    @NSManaged public var lastWorn: NSDate?
+    @NSManaged public var name: String?
+    @NSManaged public var photoData: NSData?
+    @NSManaged public var rating: Double
+    @NSManaged public var searchKey: String?
+    @NSManaged public var timesWorn: Int32
+    @NSManaged public var tintColor: NSObject?
+
+}
